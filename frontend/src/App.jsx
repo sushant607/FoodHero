576df27a321f672686c74013c43f3388722636ec
Vedant Mehra <vedantmehra20@gmail.com>
2025-03-21 23:03:59 +0530

    transaction confirm logic transactionRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FaceRecognition from './components/FaceRecognition';
-import ConfirmButton from './components/TestOrderConfirm';
+import ConfirmButtons from './components/TestOrderConfirm';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
     <>
       <div className="App">
       {/* <FaceRecognition /> */}
-      <ConfirmButton/>
+      <ConfirmButtons transactionId="67dd9d68d2867db3f7559abe" serverUserId="67dd9d21d2867db3f7559abd" receiverUserId="67dd9c2fd2867db3f7559abc" />
       </div>
      
     </>
